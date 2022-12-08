7509d236361408e25265795739324c4e102adfc1
nahum <nahum@DESKTOP-4C02UIV>
2022-12-07 20:51:55 -0600

    Commit Nahun

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/NodoM.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NodoM.java
@@ -29,7 +29,7 @@ public class NodoM {
     }
 
     void getNext2(NodoM nodoM) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        throw new UnsupportedOperationException("Not supported yet."); 
     }
     
 }
