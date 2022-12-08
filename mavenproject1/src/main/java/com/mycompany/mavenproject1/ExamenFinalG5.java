caab263fbdf69a435eb5454735f84f3c7ddb4b99
nahum <nahum@DESKTOP-4C02UIV>
2022-12-07 18:16:20 -0600

    Commit Nahun

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/ExamenFinalG5.java
@@ -0,0 +1,42 @@
+package com.mycompany.mavenproject1;
+import javax.swing.JOptionPane;
+
+public class ExamenFinalG5 {
+
+    public static void main(String[] args) {
+        int option = 0;
+        while (option != 6){
+            
+            option = Integer.parseInt(JOptionPane.showInputDialog(
+                                  "Bienvenido a Lafsa escoja una opcion:\n"
+                                   + "1. Comprar boletos\n"
+                                   + "2. Observar la cola\n"
+                                   + "3. Extraer el primero en la cola.\n"
+                                   + "4. Mostrar lista de finalizados.\n"
+                                   + "3. Ingresar maletas.\n"
+                                   + "5. Observar la lista de las maletas\n"));
+            
+            switch(option) {
+                case 1:
+
+                    break;
+                case 2:
+           
+                        
+                    break;
+                case 3:
+                  
+                    break;
+                case 4:
+                 
+                    break;
+                    case 5:
+                 
+                    break;
+                    case 6:
+                 
+                    break;
+    }
+}
+    }
+}
